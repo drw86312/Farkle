5362a2fc4a93493f3fb857fbc139105b043cecd7
drw86312 <david.warner4@gmail.com>
2014-05-21 13:55:20 -0500

    Initial Commit

---
+++ Farkle/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Farkle
+//
+//  Created by David Warner on 5/21/14.
+//  Copyright (c) 2014 Mobile Makers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
